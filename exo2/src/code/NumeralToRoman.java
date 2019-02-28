dc9702a5ebd9ee31df9151c3cb5a6650ba318d72
p18001409 <p18001409@a-f004246.salsa.univ-amu.fr>
2019-02-28 15:02:40 +0100

    Premier Commit

---
+++ exo2/src/code/NumeralToRoman.java
@@ -0,0 +1,54 @@
+package code;
+
+public class NumeralToRoman {
+	
+	public String getRoman(int nombre) {
+		String romain = null;
+		
+		if(nombre != 0) {
+			System.out.println(nombre);
+			
+			while(nombre > 0) {
+				if(nombre > 1000) {
+					nombre = nombre/1000;
+					romain = romain+"M";
+					System.out.println(nombre);
+					System.out.println("M");
+				}
+				else if(nombre > 500) {
+					nombre = nombre/500;
+					romain = romain+"D";
+					System.out.println(nombre);
+				}
+				else if(nombre > 100) {
+					nombre = nombre/100;
+					romain = romain+"C";
+					System.out.println(nombre);
+				}
+				else if(nombre > 50) {
+					nombre = nombre/50;
+					romain = romain+"L";
+					System.out.println(nombre);
+				}
+				else if(nombre > 10) {
+					nombre = nombre/10;
+					romain = romain+"X";
+					System.out.println(nombre);
+				}
+				else if(nombre > 5) {
+					nombre = nombre/5;
+					romain = romain+"V";
+					System.out.println(nombre);
+				}
+				else if(nombre > 1) {
+					nombre = nombre - 1;
+					romain = romain+"I";
+					System.out.println(nombre);
+				}
+			}
+		}
+		else System.out.println("Entrez un nombre superieur à 0");
+	 return romain;
+	}
+
+}
---
+++ exo2/test/code/NumeralToRomanTest.java
@@ -0,0 +1,27 @@
+package code;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NumeralToRomanTest {
+
+	NumeralToRoman calc;
+	
+	@Before
+	public void setUp() throws Exception {
+		calc = new NumeralToRoman();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertEquals(new String("X"), calc.getRoman(10));
+	}
+
+}
